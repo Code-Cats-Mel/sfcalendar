9be468a35f732e8fd04218708f09c3eff636d205
Nav <nkaur@aglet.app>
2024-06-04 14:28:03 +1000

    first commit

---
+++ android/app/src/main/kotlin/com/example/sfcalendar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sfcalendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() {
+  return runApp(CalendarApp());
+}
+
+/// The app which hosts the home page which contains the calendar on it.
+class CalendarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Calendar Demo', home: MyHomePage());
+  }
+}
+
+/// The hove page which hosts the calendar
+class MyHomePage extends StatefulWidget {
+  /// Creates the home page to display teh calendar widget.
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SfCalendar(
+          view: CalendarView.day,
+          monthViewSettings: const MonthViewSettings(
+              appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+        ),
+      ),
+    );
+  }
+}
